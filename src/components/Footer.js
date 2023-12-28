fdccb08fd5dbaa536cd5fafaa04bdfaeed5fbba3
Esdras Oliveira <92229240+EsdrasOliver@users.noreply.github.com>
2023-12-28 15:05:04 -0300

    components created

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>
+                <span>Pokenext</span> & copy; 2023
+            </p>
+        </footer>
+    )
+}
